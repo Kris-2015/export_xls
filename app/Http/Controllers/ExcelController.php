e80e394b1ef08f8a49dc73dd7fecc236e6fc30ff
kris batar <kris.batar@b2software.com>
2018-09-26 13:07:08 +0530

    Update ExcelController wrt file header and function header

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Exports\UsersExport;
 
+/**
+ * Class ExcelController
+ * @package App\Http\Controllers
+ * @purpose Class to export package of data into excel sheet
+ */
 class ExcelController extends Controller
 {
-    //
+    /**
+     * ExcelController constructor.
+     */
     public function __construct()
     {
         // Increase execution time so that document can be created
@@ -16,11 +23,20 @@ class ExcelController extends Controller
         ini_set('memory_limit', '-1');
     }
 
+    /**
+     * Function to return the view
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function showPage ()
     {
         return view ('excel');
     }
 
+    /**
+     * Function to export data from db
+     * @param Request $request
+     * @return mixed
+     */
     public function export(Request $request)
     {
         //return Excel::download(new UsersExport, 'users.xlsx');
@@ -29,11 +45,17 @@ class ExcelController extends Controller
         return back()->withSuccess('Export started!');
     }
 
+    /**
+     * Function to export the db data into excel file using queue
+     * @param Request $request
+     * @return \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\BinaryFileResponse
+     */
     public function exportWithQueue (Request $request)
     {
         $userExport = new UsersExport;
         $userExport->queue('users.xlsx');
 
-        return $userExport->download('users.xlsx', \Maatwebsite\Excel\Excel::XLSX);
+        return $userExport->download('users.xlsx',
+            \Maatwebsite\Excel\Excel::XLSX);
     }
 }
