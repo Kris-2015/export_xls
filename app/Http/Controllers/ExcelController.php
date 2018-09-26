a2725ef4f82ce2b0b4b5f45df7f7bca9c6bbb9fa
kris batar <kris.batar@b2software.com>
2018-09-26 15:36:37 +0530

    Update Display the grid in home page

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Exports\UsersExport;
 
@@ -29,7 +30,8 @@ class ExcelController extends Controller
      */
     public function showPage ()
     {
-        return view ('excel');
+        $users = User::getSomeRec();
+        return view ('excel', compact('users'));
     }
 
     /**
--- resources/views/excel.blade.php
+++ resources/views/excel.blade.php
@@ -6,24 +6,54 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="{{ url('bootstrap/dist/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ url('css/main.css') }}">
     </head>
     <body>
-        <div class="container">
-            <nav>
-                <h1>Export to Excel File</h1>
-            </nav>
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="col-sm-6">
-                        <a
-                            class="btn btn-primary"
-                            href="{{ route('export_file_queue') }}">
-                            Download excel sheet
-                        </a>
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="/">Export to Excel File</a>
+    </nav>
+
+    <div class="container">
+        <div class="row button-pos">
+            <div class="col-sm-12">
+                <div class="col-sm-6 button-pos-left">
+                    <a
+                        class="btn btn-primary"
+                        href="{{ route('export_file_queue') }}">
+                        Download excel sheet
+                    </a>
+                </div>
+            </div>
+            <br><br>
+            <div class="col-sm-12">
+                <div class="col-sm-9 col-sm-offset-2">
+                    <div class="table table-responsive">
+                        <table class="table table-striped">
+                            <tr>
+                                <th>id</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Mobile Number</th>
+                                <th>Email</th>
+                                <th>Country</th>
+
+                            </tr>
+                            @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->first_name }}</td>
+                                <td>{{ $user->last_name }}</td>
+                                <td>{{ $user->mobile_number }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->country }}</td>
+                            </tr>
+                            @endforeach
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
         <script src="{{ url('js/jquery-3.3.1.min.js') }}"></script>
         <script src="{{ url('bootstrap/dist/js/bootstrap.min.js') }}"></script>
     </body>
